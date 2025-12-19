64768593c60685710d9d748a20c74b1951c34342
ram7811 <ramkumarramu825@gmai.com>
2025-12-19 18:44:18 +0530

    portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,7 +17,7 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
@@ -30,7 +30,7 @@ It highlights insights on employee satisfaction, tenure, salary distribution, jo
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
@@ -40,21 +40,7 @@ The system uses facial landmark detection and the Eye Aspect Ratio (EAR) algorit
 Designed as a low-cost, non-intrusive solution, it runs efficiently on both standard cameras and embedded devices like Raspberry Pi, improving road safety through early drowsiness alerts."
               ghLink="https://github.com/ram7811/Drowzy-Alert "
             />
-          </Col> 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="HR-Attrition Analysis"
-              description="A complete HR analytics solution built using Power BI, Excel, and DAX to analyze employee turnover and identify key factors influencing attrition.
-The project includes data cleaning,Power Query transformations, DAX calculations, star schema modelling, and a fully interactive dashboard.
-It highlights insights on employee satisfaction, tenure, salary distribution, job roles, and work-life balance, helping HR teams make data-driven retention decisions."
-              ghLink="https://github.com/ram7811/HR_Attrition-Report"
-              
-            />
           </Col>
-
-
         </Row>
       </Container>
     </Container>
